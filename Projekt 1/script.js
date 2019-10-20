6aceeaede252b06237185db88c016f08a9387d69
Ligerd <fa_74@list.ru>
2019-10-20 09:34:33 +0200

    nic nie zrobione

--- Projekt 1/script.js
+++ Projekt 1/script.js
@@ -1,11 +1,38 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const lastname = document.getElementById("lastname");
-    lastname.addEventListener("input", myFunction);
+    lastname.addEventListener("input", functionforlastname);
 
-    function myFunction() {
-        console.log(lastname.value)
+    function functionforlastname(e) {
+        if (e.target.value.length<5){
+            document.getElementById('lastname').style.borderColor='#e52213';
+            // alert("problem w name")
+        }else {
+            document.getElementById('lastname').style.border='solid';
+        }
+    }
+
+    const name=document.getElementById("name");
+    name.addEventListener("input",functionname);
+
+    function functionname(e) {
+        if (e.target.value.length<5){
+            document.getElementById('name').style.borderColor='#e52213';
+           // alert("problem w name")
+        }else {
+            document.getElementById('name').style.border='solid';
+        }
+    }
+
+    const loigin= document.getElementById("login");
+    login.addEventListener("input", functionlogin);
+
+    function functionlogin(e) {
+        if (e.target.value.length<5){
+            document.getElementById('login').style.borderColor='#e52213';
+            // alert("problem w name")
+        }else {
+            document.getElementById('login').style.border='solid';
+        }
     }
 
 });
