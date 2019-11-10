227f2a6a6a32772274b39ebba7be03ce3d43722c
Ligerd <fa_74@list.ru>
2019-11-10 16:01:52 +0100

    Coś tam działa

--- Projekt 1/serwer/app/main.py
+++ Projekt 1/serwer/app/main.py
@@ -41,12 +41,29 @@ def singin():
     else:
         return jsonify({'error': ' Użytkownika z takim loginem nie ma w bazie'})
 
+
+app.config["IMAGE_UPLOADS"]="static/img"
+app.config["ALLOWED_FORMAT"]=["PDF"]
+
+def check_file(filename):
+    if not "." in filename:
+        return False
+
+    ext=filename.rsplit(".",1)[1]
+
+    if ext.upper() in app.config["ALLOWED_FORMAT"]:
+        return True
+    else:
+        return False
+
 @app.route("/upload-image",methods=["GET","POST"])
 def upload_image():
     if request.method=="POST":
         if request.files:
             image=request.files["pdf"]
-            print(image)
+            if not check_file(image.filename):
+                return redirect(request.url)
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"],image.filename))
             return redirect(request.url)
     return redirect('http://localhost:3001/upload-file')
 
