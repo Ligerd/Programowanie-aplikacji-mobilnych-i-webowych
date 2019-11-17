7feea05e4a79cceb184bb475e1af0d65fdc5d306
Ligerd <fa_74@list.ru>
2019-11-17 20:26:16 +0100

    cooki

--- Projekt 1/serwer/app/main.py
+++ Projekt 1/serwer/app/main.py
@@ -1,13 +1,17 @@
-from flask import session, Flask, Blueprint, request, Response, jsonify,redirect, g , url_for
+from flask import session, Flask, Blueprint, request, Response, jsonify,redirect, g , url_for, make_response
 from flask import render_template
 import os
 import sys
 import redis
 import hashlib
 
+POST = "POST"
+GET = "GET"
+SESSION_ID = "session-id"
 
 app = Flask(__name__)
 db = redis.Redis(host='redis', port=6379, decode_responses=True)
+db.flushdb()
 
 DIR_PATH = "files/"
 FILE_COUNTER = "file_counter"
@@ -19,6 +23,7 @@ FILENAMES = "filenames"
 @app.route('/')
 def show_articles():
     files = db.hvals(FILENAMES)
+    #return redirect(url_for("http://localhost:3001/upload-file", my_files=files))
     return render_template("uploadfileSerwer.html", my_files = files)
 
 @app.after_request
@@ -48,29 +53,28 @@ def singin():
     password = request.form["password"]
     if lg in bazadanych:
         if bazadanych[lg] == int(password):
-            return redirect("http://localhost:3001/upload-file")
-        else:
+            name_hash = hashlib.sha512(lg.encode("utf-8")).hexdigest()
+            db.set(SESSION_ID, name_hash)
+            response = make_response(render_template("uploadfileSerwer.html"))
+            response.set_cookie(SESSION_ID, name_hash, max_age=30, secure=True, httponly=True)
+            return response
+
+    else:
             return redirect(request.url)
 
 
 app.config["IMAGE_UPLOADS"]="static/img"
 app.config["ALLOWED_FORMAT"]=["PDF"]
 
-def check_file(filename):
-    if not "." in filename:
-        return False
-    ext=filename.rsplit(".",1)[1]
-    if ext.upper() in app.config["ALLOWED_FORMAT"]:
-        return True
-    else:
-        return False
-
 @app.route("/upload-image",methods=["GET","POST"])
 def upload_image():
     if request.method=="POST":
-        f = request.files["pdf"]
-        save_file(f)
-        return redirect(url_for("show_articles"))
+        name_hash=request.cookies.get(SESSION_ID)
+        dbname_hash=db.get("SESSION_ID")
+        if(name_hash==dbname_hash):
+            f = request.files["pdf"]
+            save_file(f)
+            return redirect(url_for("show_articles"))
 
 def save_file(file_to_save):
     if(len(file_to_save.filename) > 0):
